a597d5eaa674b9f248b0c556d36605fb995f88d8
Lyubomir Georgiev <lygeorgi@cisco.com>
2023-06-01 19:28:53 -0400

    Adding nonce update logic

--- scrape_soqfeu.py
+++ scrape_soqfeu.py
@@ -1,15 +1,25 @@
 from typing import Dict
+import re
 import json
 import urllib.request
 import urllib.parse
 
 def get_json()->Dict:
     url = 'https://sopfeu.qc.ca/wp-admin/admin-ajax.php'
+    url_main = 'https://sopfeu.qc.ca/'
+
+    req_main = urllib.request.Request(url_main, method='GET')
+    with urllib.request.urlopen(req_main) as r:
+        html = r.read().decode('utf-8')
+    start_re = re.compile(r'ajaxNonce":')
+    end = ',"adminAjax"'
+    new_nonce = start_re.split(html)[1].split(end)[0].strip('\"')
+
     data = {
         'type': 'activites',
         'zone': 1,
         'action': 'wplr_push_popup_stats',
-        'nonce': '738609c2ce',
+        'nonce': new_nonce,
     }
 
     data = urllib.parse.urlencode(data).encode('utf-8')
